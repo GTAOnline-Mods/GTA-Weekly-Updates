a8b92ab9e591f8fb959d032113e3c14719166e18
RaviAnand Mohabir <moravrav@gmail.com>
2020-08-12 11:49:21 +0200

    Add firebase context with auth

---
+++ src/firebase/index.ts
@@ -0,0 +1,7 @@
+import FirebaseContext from "./context";
+import Firebase from "./firebase";
+
+export default Firebase;
+
+export { FirebaseContext };
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Firebase, { FirebaseContext } from "./firebase";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={new Firebase()}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </FirebaseContext.Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
