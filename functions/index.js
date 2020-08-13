bc0e57372e0b6bc260d4e0eec89e2387751a9e99
RaviAnand Mohabir <moravrav@gmail.com>
2020-08-13 08:50:32 +0200

    Add getSubredditIcon function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const reddit = require("./reddit");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +8,12 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.getSubredditIcon = functions.https.onCall((data, context) => {
+  return Promise.all(reddit.subreddit.community_icon, reddit.subreddit.icon_img)
+    .then((community_icon, icon_img) => ({
+      community_icon: community_icon,
+      icon_img: icon_img,
+    }))
+    .catch((error) => ({ error }));
+});
