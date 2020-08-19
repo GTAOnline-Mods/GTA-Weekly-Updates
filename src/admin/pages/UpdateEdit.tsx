77d244d0acff36a43b9d098b42a1c35ddd5cedee
Dan6erbond <moravrav@gmail.com>
2020-08-19 16:10:12 +0200

    Add menus to add update new, sales and twitch prime items Add default sale amount of 10% and enable removal of items

--- src/admin/pages/UpdateEdit.tsx
+++ src/admin/pages/UpdateEdit.tsx
@@ -1,26 +1,20 @@
 import firebase from "firebase";
 import _ from "lodash";
 import React from "react";
-import { Container, Form, Spinner } from "react-bootstrap";
+import { Col, Container, Form, ListGroup, Spinner } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import { connect } from "react-redux";
 import { RouteComponentProps } from "react-router";
 import { bindActionCreators, compose, Dispatch } from "redux";
+import SearchInput from "../../components/SearchInput";
 import Firebase, { withFirebase } from "../../Firebase";
-import Update from "../../models/update";
+import Update, { SaleItem, UpdateItem } from "../../models/update";
 import { Vehicle } from "../../models/vehicle";
 import { RootState } from "../../store";
 import { setUpdate, setUpdates } from "../../store/Updates";
 import { setVehicles } from "../../store/Vehicles";
 import "./UpdateEdit.scss";
-
-const shops = [
-  "Legendary Motorsports",
-  "Arena War",
-  "Southern San Andreas Super Autos",
-  "Warstock Cache & Carry",
-  "Benny's Original Motor Works",
-];
+import UpdateItemEditor from "./UpdateItemEditor";
 
 interface UpdateEditMatch {
   id?: string;
@@ -41,7 +35,7 @@ interface UpdateEditState {
   loading: boolean;
 }
 
-class VehicleEdit extends React.Component<UpdateEditProps, UpdateEditState> {
+class UpdateEdit extends React.Component<UpdateEditProps, UpdateEditState> {
   constructor(props: UpdateEditProps) {
     super(props);
 
@@ -89,11 +83,50 @@ class VehicleEdit extends React.Component<UpdateEditProps, UpdateEditState> {
     }
   }
 
-  setValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setValue = (name: string, value: any) => {
     this.setState({
       update: {
         ...this.state.update!!,
-        [event.target.name]: event.target.value,
+        [name]: value,
+      },
+    });
+    this.saveUpdate();
+  };
+
+  setDate = (date: Date) => {
+    this.setState({
+      update: {
+        ...this.state.update!,
+        date,
+      },
+    });
+    this.saveUpdate();
+  };
+
+  setItem = (key: keyof Update, item: UpdateItem | SaleItem) => {
+    this.setState({
+      update: {
+        ...this.state.update!,
+        [key]: [
+          ...this.state.update![key].filter(
+            (i: UpdateItem) => item.id !== i.id
+          ),
+          item,
+        ],
+      },
+    });
+    this.saveUpdate();
+  };
+
+  deleteItem = (key: keyof Update, item: UpdateItem | SaleItem) => {
+    this.setState({
+      update: {
+        ...this.state.update!,
+        [key]: [
+          ...this.state.update![key].filter(
+            (i: UpdateItem) => item.id !== i.id
+          ),
+        ],
       },
     });
     this.saveUpdate();
@@ -103,22 +136,26 @@ class VehicleEdit extends React.Component<UpdateEditProps, UpdateEditState> {
     if (this.state.update) {
       const { docRef, ...u } = this.state.update;
 
+      const update = {
+        new: [...u.new.map((i) => i.docRef)],
+        podium: u.podium?.docRef,
+        sale: [...u.sale.map((i) => ({ item: i.docRef, amount: i.amount }))],
+        twitchPrime: [
+          ...u.twitchPrime.map((i) => ({
+            item: i.docRef,
+            amount: i.amount,
+          })),
+        ],
+        date: firebase.firestore.Timestamp.fromDate(u.date),
+      };
+
       this.setState({
         loading: true,
       });
 
       if (docRef) {
-        docRef
-          .update({
-            new: u.new.map((i) => i.docRef),
-            podium: u.podium?.docRef,
-            sale: u.sale.map((i) => ({ item: i.docRef, amount: i.amount })),
-            twitchPrime: u.twitchPrime.map((i) => ({
-              item: i.docRef,
-              amount: i.amount,
-            })),
-            date: firebase.firestore.Timestamp.fromDate(u.date),
-          })
+        docRef!
+          .update(update)
           .then(() => {
             this.props.setUpdate(this.state.update!!);
             this.setState({
@@ -157,20 +194,106 @@ class VehicleEdit extends React.Component<UpdateEditProps, UpdateEditState> {
       <Container fluid>
         {update ? (
           <div>
-            <h2>{update.date.toLocaleDateString()}</h2>
-            <Form className="mt-2">Test.</Form>
-            <DatePicker
-              selected={update.date}
-              onChange={(date: Date) => {
-                this.setState({
-                  update: {
-                    ...update!,
-                    date: date,
-                  },
-                });
-                this.saveUpdate();
-              }}
-            />
+            <h1 className="pb-4 mb-4">{update.date.toLocaleDateString()}</h1>
+            <Form className="mt-4 pt-4" onSubmit={(e) => e.preventDefault()}>
+              <DatePicker
+                className="mb-2 mt-4"
+                selected={update.date}
+                onChange={this.setDate}
+              />
+              <Form.Row className="my-2">
+                <Form.Group as={Col} md="6" sm="12">
+                  <Form.Label>Podium</Form.Label>
+                  <SearchInput
+                    options={this.props.vehicles.map((v) => ({
+                      label: `${v.manufacturer} ${v.name}`,
+                      value: v,
+                      id: v.docRef!.id,
+                    }))}
+                    onSelect={(option) => {
+                      this.setValue("podium", option.value);
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group as={Col} md="6" sm="12">
+                  <Form.Label>New</Form.Label>
+                  <SearchInput
+                    multi
+                    options={this.props.vehicles.map((v) => ({
+                      label: v.name,
+                      value: v,
+                      id: v.docRef!.id,
+                    }))}
+                    onSelect={(option) => this.setItem("new", option.value)}
+                  />
+                  <ListGroup className="mt-2">
+                    {this.state.update?.new?.map((i) => (
+                      <UpdateItemEditor
+                        item={i}
+                        key={i.docRef!.id}
+                        setItem={(item) => this.setItem("new", item)}
+                        deleteItem={() => this.deleteItem("new", i)}
+                      />
+                    ))}
+                  </ListGroup>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row className="my-2">
+                <Form.Group as={Col} md="6" sm="12">
+                  <Form.Label>Sale</Form.Label>
+                  <SearchInput
+                    multi
+                    options={this.props.vehicles.map((v) => ({
+                      label: `${v.manufacturer} ${v.name}`,
+                      value: v,
+                      id: v.docRef!.id,
+                    }))}
+                    onSelect={(option) =>
+                      this.setItem("sale", { ...option.value, amount: 10 })
+                    }
+                  />
+                  <ListGroup className="mt-2">
+                    {this.state.update?.sale?.map((i) => (
+                      <UpdateItemEditor
+                        item={i}
+                        sale
+                        key={i.docRef!.id}
+                        setItem={(item) => this.setItem("sale", item)}
+                        deleteItem={() => this.deleteItem("sale", i)}
+                      />
+                    ))}
+                  </ListGroup>
+                </Form.Group>
+                <Form.Group as={Col} md="6" sm="12">
+                  <Form.Label>Twitch Prime</Form.Label>
+                  <SearchInput
+                    multi
+                    options={this.props.vehicles.map((v) => ({
+                      label: `${v.manufacturer} ${v.name}`,
+                      value: v,
+                      id: v.docRef!.id,
+                    }))}
+                    onSelect={(option) =>
+                      this.setItem("twitchPrime", {
+                        ...option.value,
+                        amount: 10,
+                      })
+                    }
+                  />
+                  <ListGroup className="mt-2">
+                    {this.state.update?.twitchPrime?.map((i) => (
+                      <UpdateItemEditor
+                        item={i}
+                        sale
+                        key={i.docRef!.id}
+                        setItem={(item) => this.setItem("twitchPrime", item)}
+                        deleteItem={() => this.deleteItem("twitchPrime", i)}
+                      />
+                    ))}
+                  </ListGroup>
+                </Form.Group>
+              </Form.Row>
+            </Form>
             {loading && (
               <div className="d-flex flex-row-reverse">
                 <Spinner animation="border" role="status" className="mr-4 mt-2">
@@ -207,4 +330,4 @@ const mapStateToProps = (state: RootState) => ({
 export default compose(
   withFirebase,
   connect(mapStateToProps, mapDispatchToProps)
-)(VehicleEdit) as any;
+)(UpdateEdit) as any;
