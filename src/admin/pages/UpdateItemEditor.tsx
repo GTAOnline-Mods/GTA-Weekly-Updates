836159bcf2bf0d1da94f880cd40460d20cbb00dd
Dan6erbond <moravrav@gmail.com>
2020-08-19 16:10:33 +0200

    Create update item editor component with inputs for sale amount and delete option

---
+++ src/admin/pages/UpdateItemEditor.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Button, FormControl, InputGroup, ListGroup } from "react-bootstrap";
+import { SaleItem, UpdateItem } from "../../models/update";
+
+interface UpdateItemEditorProps {
+  item: UpdateItem | SaleItem;
+  sale?: boolean;
+  setItem: (item: UpdateItem | SaleItem) => void;
+  deleteItem: () => void;
+}
+
+function UpdateItemEditor({
+  item,
+  sale,
+  setItem,
+  deleteItem,
+}: UpdateItemEditorProps) {
+  return (
+    <ListGroup.Item className="p-0">
+      <InputGroup>
+        <InputGroup.Prepend>
+          <InputGroup.Text className="w-100">{item.name}</InputGroup.Text>
+        </InputGroup.Prepend>
+        {sale && (
+          <FormControl
+            value={(item as SaleItem).amount}
+            style={{ flex: "100000", maxWidth: "80px" }}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setItem({
+                ...item,
+                amount: parseInt(event.target.value),
+              })
+            }
+            aria-label={sale ? "sale-item" : "update-item"}
+          />
+        )}
+        <InputGroup.Append>
+          {sale && <InputGroup.Text>%</InputGroup.Text>}
+          <Button
+            variant="secondary"
+            onClick={deleteItem}
+            style={{
+              backgroundColor: "#e9ecef",
+              borderColor: "#ced4da",
+              color: "black",
+            }}
+          >
+            Delete
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </ListGroup.Item>
+  );
+}
+
+export default UpdateItemEditor;
