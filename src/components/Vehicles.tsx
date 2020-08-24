f84a99f101e696b23248afef0a3672eeeb5f71a0
Dan6erbond <moravrav@gmail.com>
2020-08-24 07:56:09 +0200

    Squashed commit of the following:
    commit 63bddff85de85cde570795c10ef46a0f7e235e60
    Author: Dan6erbond <moravrav@gmail.com>
    Date:   Mon Aug 24 07:55:11 2020 +0200
    
        Add vehicles refresh button and loading spinner
    
    commit ef7cd7296628d8f005fe3707352345f0b75302b4
    Author: Dan6erbond <moravrav@gmail.com>
    Date:   Mon Aug 24 07:32:03 2020 +0200
    
        Simplfy function call
    
    commit 4a98d4134ce0e000bc00952ec0f105bd502004ad
    Author: Dan6erbond <moravrav@gmail.com>
    Date:   Mon Aug 24 07:26:34 2020 +0200
    
        Load single vehicle in detail views

--- src/components/Vehicles.tsx
+++ src/components/Vehicles.tsx
@@ -1,4 +1,4 @@
-import { faEdit, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faSearch, faSync } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import {
@@ -6,7 +6,8 @@ import {
   Container,
   FormControl,
   InputGroup,
-  ListGroup
+  ListGroup,
+  Spinner
 } from "react-bootstrap";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
@@ -32,13 +33,16 @@ function Vehicles({
   admin,
 }: VehiclesProps) {
   const [search, setSearch] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+
+  async function getVehicles() {
+    setLoading(true);
+    const v = await firebase!.getVehicles();
+    setVehicles(v);
+    setLoading(false);
+  }
 
   React.useEffect(() => {
-    async function getVehicles() {
-      const v = await firebase!.getVehicles();
-      setVehicles(v);
-    }
-
     if (!vehicles.length) {
       getVehicles();
     }
@@ -49,51 +53,74 @@ function Vehicles({
     <Container fluid className="p-2">
       <h1>Vehicles</h1>
       <br />
-      <InputGroup className="mb-3">
-        <FormControl
-          placeholder="Vehicle Name"
-          aria-label="Vehicle Name"
-          aria-describedby="search"
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setSearch(event.target.value)
-          }
-        />
-        <InputGroup.Append>
-          <InputGroup.Text id="search">
-            <FontAwesomeIcon icon={faSearch} />
-          </InputGroup.Text>
-        </InputGroup.Append>
-      </InputGroup>
+      <div className="d-flex mb-3">
+        <InputGroup>
+          <FormControl
+            placeholder="Vehicle Name"
+            aria-label="Vehicle Name"
+            aria-describedby="search"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setSearch(event.target.value)
+            }
+          />
+          <InputGroup.Append>
+            <InputGroup.Text id="search">
+              <FontAwesomeIcon icon={faSearch} />
+            </InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        <Button
+          variant="secondary"
+          onClick={getVehicles}
+          style={{
+            backgroundColor: "#e9ecef",
+            borderColor: "#ced4da",
+            color: "black",
+          }}
+        >
+          <FontAwesomeIcon icon={faSync} />
+        </Button>
+      </div>
       <br />
-      <ListGroup>
-        {(search
-          ? vehicles.filter((v) =>
-              (v.manufacturer + " " + v.name).toLowerCase().includes(search.toLowerCase())
-            )
-          : vehicles
-        ).map((vehicle) => (
-          <ListGroup.Item
-            action
-            as={Link}
-            to={"/vehicles/" + vehicle.docRef?.id}
-            key={vehicle.docRef!.id}
-            className="d-flex justify-content-between align-items-center"
-          >
-            <span>
-              <b>{vehicle.manufacturer}</b> {vehicle.name}
-            </span>
-            {admin && isAdmin && (
-              <Button
-                variant="link"
-                as={Link}
-                to={"/admin/vehicles/edit/" + vehicle.docRef?.id}
-              >
-                <FontAwesomeIcon icon={faEdit} />
-              </Button>
-            )}
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+      {loading ? (
+        <div className="d-flex justify-content-center">
+          <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </div>
+      ) : (
+        <ListGroup>
+          {(search
+            ? vehicles.filter((v) =>
+                (v.manufacturer + " " + v.name)
+                  .toLowerCase()
+                  .includes(search.toLowerCase())
+              )
+            : vehicles
+          ).map((vehicle) => (
+            <ListGroup.Item
+              action
+              as={Link}
+              to={"/vehicles/" + vehicle.docRef?.id}
+              key={vehicle.docRef!.id}
+              className="d-flex justify-content-between align-items-center"
+            >
+              <span>
+                <b>{vehicle.manufacturer}</b> {vehicle.name}
+              </span>
+              {admin && isAdmin && (
+                <Button
+                  variant="link"
+                  as={Link}
+                  to={"/admin/vehicles/edit/" + vehicle.docRef?.id}
+                >
+                  <FontAwesomeIcon icon={faEdit} />
+                </Button>
+              )}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      )}
       <br />
       {admin && isAdmin && (
         <div className="d-flex flex-row-reverse">
