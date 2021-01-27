0f6660059a5213ce6690d83f0d91bf9cafa841be
Dan6erbond <moravrav@gmail.com>
2021-01-27 16:40:33 +0100

    Create cloud function to update documents onWrite with `createdAt` and `updatedAt` fields (#13)

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,72 @@
 const functions = require("firebase-functions");
-const reddit = require("./reddit");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.myFunction = functions.firestore
+  .document("{collection}/{document=**}")
+  .onWrite(async (change, context) => {
+    const ignoreCollections = ["configs", "users"];
 
-exports.getSubredditIcon = functions.https.onCall((data, context) => {
-  return Promise.all(reddit.subreddit.community_icon, reddit.subreddit.icon_img)
-    .then((community_icon, icon_img) => ({
-      community_icon: community_icon,
-      icon_img: icon_img,
-    }))
-    .catch((error) => ({ error }));
-});
+    // if one of the ignore collections
+    if (ignoreCollections.indexOf(context.params.collection) !== 1) {
+      return null;
+    }
+
+    // simplify event types
+    const createDoc = change.after.exists && !change.before.exists;
+    const updateDoc = change.before.exists && change.after.exists;
+    const deleteDoc = change.before.exists && !change.after.exists;
+
+    if (deleteDoc) {
+      return null;
+    }
+
+    // simplify input data
+    const after = change.after.exists ? change.after.data() : null;
+    const before = change.before.exists ? change.before.data() : null;
+
+    // prevent update loops from triggers
+    const canUpdate = () => {
+      // if update trigger
+      if (before.updatedAt && after.updatedAt) {
+        if (after.updatedAt._seconds !== before.updatedAt._seconds) {
+          return false;
+        }
+      }
+      // if create trigger
+      if (!before.createdAt && after.createdAt) {
+        return false;
+      }
+      return true;
+    };
+
+    // add createdAt
+    if (createDoc) {
+      return change.after.ref
+        .set(
+          {
+            createdAt: admin.firestore.FieldValue.serverTimestamp(),
+          },
+          { merge: true }
+        )
+        .catch((e: any) => {
+          console.log(e);
+          return false;
+        });
+    }
+
+    // add updatedAt
+    if (updateDoc && canUpdate()) {
+      return change.after.ref
+        .set(
+          {
+            updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+          },
+          { merge: true }
+        )
+        .catch((e: any) => {
+          console.log(e);
+          return false;
+        });
+    }
+    return null;
+  });
